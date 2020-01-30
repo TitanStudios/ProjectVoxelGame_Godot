730f5387172a0eb6b333233c379ee66189c503dd
Carlos Santamaria <titan-1719@hotmail.com>
2020-01-29 22:01:30 -0500

    Actualizacion a la nueva version Godot
    Godot 3.2

--- TestScript.cs
+++ TestScript.cs
@@ -3,21 +3,21 @@ using System;
 
 public class YourCustomClass : Node
 {
-    // Member variables here, example:
-    private int a = 2;
-    private string b = "textvar";
+	// Member variables here, example:
+	private int a = 2;
+	private string b = "textvar";
 
-    public override void _Ready()
-    {
-        // Called every time the node is added to the scene.
-        // Initialization here.
-        GD.Print("Hello from C# to Godot :)");
+	public override void _Ready()
+	{
+		// Called every time the node is added to the scene.
+		// Initialization here.
+		GD.Print("Hello from C# to Godot :)");
 		GD.Print("Probando con los gestores de Git");
-    }
+	}
 
-    public override void _Process(float delta)
-    {
-        // Called every frame. Delta is time since the last frame.
-        // Update game logic here.
-    }
+	public override void _Process(float delta)
+	{
+		// Called every frame. Delta is time since the last frame.
+		// Update game logic here.
+	}
 }
