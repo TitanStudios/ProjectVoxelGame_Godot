b6dc253b88a2c31f26f3a7b766e8bbdd8b1df9a0
TitanStudios <titan-1719@hotmail.com>
2020-05-29 16:59:31 -0500

    Actualizacion del Script Test
    Probando cambios

--- TestScript.cs
+++ TestScript.cs
@@ -1,23 +1,21 @@
 using Godot;
 using System;
 
-public class YourCustomClass : Node
+public class TestScript : Node
 {
-	// Member variables here, example:
-	private int a = 2;
-	private string b = "textvar";
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
 
+	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		// Called every time the node is added to the scene.
-		// Initialization here.
-		GD.Print("Hello from C# to Godot :)");
-		GD.Print("Probando con los gestores de Git");
+		
 	}
 
-	public override void _Process(float delta)
-	{
-		// Called every frame. Delta is time since the last frame.
-		// Update game logic here.
-	}
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
 }
