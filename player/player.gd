3cdcec84c0f1657d9cc7c42c77f9917032455943
TitanStudios <titan-1719@hotmail.com>
2021-02-18 00:51:11 -0500

    Haciendo Tutorial 1
    Haciendo el curso de udemy y modificando el proyecto.

---
+++ player/player.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+const RIGHT : int = 1
+const LEFT : int = -1
+
+export var speed : int = 4
+
+"""
+func _input(event):
+	if event.is_action_pressed("mover_derecha"):
+		position.x = position.x + speed
+	if event.is_action_pressed("mover_izquierda"):
+		position.x = position.x - speed
+"""
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if Input.is_action_pressed("mover_derecha"):
+		position.x += speed
+	if Input.is_action_pressed("mover_izquierda"):
+		position.x -= speed
