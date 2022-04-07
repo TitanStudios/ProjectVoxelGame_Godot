e7fcaed4e99af96e9e0499dac08b70d9e14030ba
Carlos Santamaria <titan-1719@hotmail.com>
2022-04-07 00:24:14 -0500

    Actualizado a la version 4 alpha
    probando la version 4 alpha de godot

--- player/player.gd
+++ player/player.gd
@@ -1,17 +1,17 @@
-extends KinematicBody2D
+extends CharacterBody2D
 
 const RIGHT : int = 1
 const LEFT : int = -1
 
-export var speed : int = 4
+var speed : int = 4
 
-"""
+
 func _input(event):
 	if event.is_action_pressed("mover_derecha"):
 		position.x = position.x + speed
 	if event.is_action_pressed("mover_izquierda"):
 		position.x = position.x - speed
-"""
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,7 +19,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+func _physics_process(_delta):
 	if Input.is_action_pressed("mover_derecha"):
 		position.x += speed
 	if Input.is_action_pressed("mover_izquierda"):
